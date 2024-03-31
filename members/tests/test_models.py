08c465b44a464a3e17a3e78ab21eef1c51da88b8
mizaelanthony <mizaelanthony07@gmail.com>
2024-03-31 15:34:12 +0300

    ref-organize-all-test

--- members/tests/test_models.py
+++ members/tests/test_models.py
@@ -1,20 +1,23 @@
 from members.models import CustomUser
 import pytest
 
-def test_user_with_default_photo(custom_user_fixture):
-  data = custom_user_fixture.user_with_default_photo()
-  user = CustomUser.objects.create_user(**data)
+# NOTE : class/method name must start with "test"
+class TestCustomUser:
 
-  assert user.username == "T.Test"
-  assert user.check_password("123456") is True
-  
-def test_user_without_email(custom_user_fixture):
-  data = custom_user_fixture.user_without_email()
-  with pytest.raises(TypeError) as e:
-    CustomUser.objects.create_user(**data)
+  @pytest.fixture(autouse=True)
+  def setup_fixture(self, custom_user_fixture):
+    self.custom_user_fixture = custom_user_fixture
 
-  assert "CustomUserManager.create_user() missing 1 required positional argument: 'email'" == str(e.value)
+  def test_user_with_default_photo(self, db):
+    data = self.custom_user_fixture.user_with_default_photo()
+    user = CustomUser.objects.create_user(**data)
 
-  
+    assert user.username == "T.Test"
+    assert user.check_password("123456") is True
+    
+  def test_user_without_email(self, db):
+    data = self.custom_user_fixture.user_without_email()
+    with pytest.raises(TypeError) as e:
+      CustomUser.objects.create_user(**data)
 
-
+    assert "CustomUserManager.create_user() missing 1 required positional argument: 'email'" == str(e.value)
