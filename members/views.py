e9dd86810aa15e5d1f0722e3f1958eae8e9dab13
mizaelanthony <mizaelanthony07@gmail.com>
2024-04-02 21:52:18 +0300

    feat-logout

--- members/views.py
+++ members/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 # NOTE :
 # With CustomUserBackend we can use form.username as email field in login template
 # No need to override AuthenticationForm
-class CustomUserLoginView(LoginView):
+class CustomLoginView(LoginView):
   template_name = "members/login.html"
 
+class CustomLogoutView(LogoutView):
+  template_name = "members/logged_out.html"
