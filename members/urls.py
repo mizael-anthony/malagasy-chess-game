6cb62e3ef5edae9019fd116b47210822003906ac
Mizael Anthony <mizaelanthony07@gmail.com>
2024-04-02 21:53:59 +0300

    feat-logout (#8)

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomUserLoginView
+from .views import CustomLoginView, CustomLogoutView
 
 app_name = 'members'
 
 urlpatterns = [
-    path('login/', CustomUserLoginView.as_view(), name='login')
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
