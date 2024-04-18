4af0b08714c04c745980fd586006bad736f593b0
Mizael Anthony <mizaelanthony07@gmail.com>
2024-04-18 20:21:53 +0300

    Ref refonte interface (#19)
    * ref-refonte-interface
    
    * ref-save-requirements

--- club/views.py
+++ club/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
 
 # Create your views here.
-class ClubView(generic.View):
+
+class ClubView(LoginRequiredMixin, View):
+    login_url = reverse_lazy("members:login")
     def get(self, request):
-        return render(request, 'club/home.html')
+        context = {
+            "user": request.user
+        }
+        return render(request, 'club/home.html', context=context)
     
